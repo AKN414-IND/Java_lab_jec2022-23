c0d049c616c9bcfbf0504283cfc4b7fdb9349cdf
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2022-09-27 20:40:04 +0530

    Update PrimeScan.java

--- PrimeScan.java
+++ PrimeScan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Program7 {
+public class PrimeScan {
     public static void main(String args[]) {
         int i, f = 0;
         System.out.print("Enter the number:");
